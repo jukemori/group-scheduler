e04493a56f891cba4606855ff774f77fe0f7ec0d
Jun Ukemori <jukemori@gmail.com>
2024-08-31 16:20:55 +0900

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,44 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import {
+  Week, Month, Agenda, ScheduleComponent, ViewsDirective, ViewDirective, EventSettingsModel, ResourcesDirective, ResourceDirective, Inject, Resize, DragAndDrop
+} from '@syncfusion/ej2-react-schedule';
+import { timelineResourceData } from './datasource';
+// import { registerLicense } from '@syncfusion/ej2-base';
+
 
 export default function Home() {
+  // registerLicense(process.env.SYNCFUSION_LICENSE || '');
+  const eventSettings: EventSettingsModel = { dataSource: timelineResourceData }
+  const group = { byGroupID: false, resources: ['Projects', 'Categories'] }
+
+  const projectData: Object[] = [
+    { text: 'PROJECT 1', id: 1, color: '#cb6bb2' },
+    { text: 'PROJECT 2', id: 2, color: '#56ca85' },
+    { text: 'PROJECT 3', id: 3, color: '#df5286' },
+  ];
+  const categoryData: Object[] = [
+    { text: 'Development', id: 1, color: '#1aaa55' },
+    { text: 'Testing', id: 2, color: '#7fa900' }
+  ];
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    <>
+      <h2>Syncfusion React Schedule Component</h2>
+      <ScheduleComponent width='100%' height='550px' currentView='Month' selectedDate={new Date(2018, 3, 4)} eventSettings={eventSettings} group={group} >
+        <ViewsDirective>
+          <ViewDirective option='Week' />
+          <ViewDirective option='Month' />
+          <ViewDirective option='Agenda' />
+        </ViewsDirective>
+        <ResourcesDirective>
+          <ResourceDirective field='ProjectId' title='Choose Project' name='Projects' allowMultiple={false}
+            dataSource={projectData} textField='text' idField='id' colorField='color'>
+          </ResourceDirective>
+          <ResourceDirective field='TaskId' title='Category' name='Categories' allowMultiple={true}
+            dataSource={categoryData} textField='text' idField='id' colorField='color'>
+          </ResourceDirective>
+        </ResourcesDirective>
+        <Inject services={[Week, Month, Agenda, Resize, DragAndDrop]} />
+      </ScheduleComponent>
+    </>
+  )
 }
