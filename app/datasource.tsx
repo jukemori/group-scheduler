13590dda1a2ec2f596670120d402e0cfc5cac139
Jun Ukemori <jukemori@gmail.com>
2024-08-31 17:47:54 +0900

    modify data

--- app/datasource.tsx
+++ app/datasource.tsx
@@ -2,74 +2,65 @@ export let timelineResourceData: Object[] = [
   {
       Id: 61,
       Subject: 'Decoding',
-      StartTime: new Date(2018, 3, 4, 9, 30),
+      StartTime: new Date(2018, 3, 4, 9, 30),  // April 4th
       EndTime: new Date(2018, 3, 4, 10, 30),
       IsAllDay: false,
-      ProjectId: 2,
-      TaskId: 2
+      OwnerId: 1,
   }, {
       Id: 62,
       Subject: 'Bug Automation',
-      StartTime: new Date(2018, 3, 4, 13, 30),
-      EndTime: new Date(2018, 3, 4, 16, 30),
+      StartTime: new Date(2018, 3, 6, 13, 30),  // April 6th
+      EndTime: new Date(2018, 3, 6, 16, 30),
       IsAllDay: false,
-      ProjectId: 2,
-      TaskId: 1
+      OwnerId: 2,
   }, {
       Id: 63,
       Subject: 'Functionality testing',
-      StartTime: new Date(2018, 3, 4, 9),
-      EndTime: new Date(2018, 3, 4, 10, 30),
+      StartTime: new Date(2018, 3, 8, 9),  // April 8th
+      EndTime: new Date(2018, 3, 8, 10, 30),
       IsAllDay: false,
-      ProjectId: 1,
-      TaskId: 1
+      OwnerId: 3,
   }, {
       Id: 64,
       Subject: 'Resolution-based testing',
-      StartTime: new Date(2018, 3, 4, 12),
-      EndTime: new Date(2018, 3, 4, 13),
+      StartTime: new Date(2018, 3, 10, 12),  // April 10th
+      EndTime: new Date(2018, 3, 10, 13),
       IsAllDay: false,
-      ProjectId: 1,
-      TaskId: 1
+      OwnerId: 1,
   }, {
       Id: 65,
       Subject: 'Test report Validation',
-      StartTime: new Date(2018, 3, 4, 15),
-      EndTime: new Date(2018, 3, 4, 18),
+      StartTime: new Date(2018, 3, 12, 15),  // April 12th
+      EndTime: new Date(2018, 3, 12, 18),
       IsAllDay: false,
-      ProjectId: 1,
-      TaskId: 1
+      OwnerId: 2,
   }, {
       Id: 66,
       Subject: 'Test case correction',
-      StartTime: new Date(2018, 3, 4, 14),
-      EndTime: new Date(2018, 3, 4, 16),
+      StartTime: new Date(2018, 3, 14, 14),  // April 14th
+      EndTime: new Date(2018, 3, 14, 16),
       IsAllDay: false,
-      ProjectId: 1,
-      TaskId: 2
+      OwnerId: 3,
   }, {
       Id: 67,
       Subject: 'Bug fixing',
-      StartTime: new Date(2018, 3, 4, 14, 30),
-      EndTime: new Date(2018, 3, 4, 18, 30),
+      StartTime: new Date(2018, 3, 16, 14, 30),  // April 16th
+      EndTime: new Date(2018, 3, 16, 18, 30),
       IsAllDay: false,
-      ProjectId: 2,
-      TaskId: 2
+      OwnerId: 1,
   }, {
       Id: 68,
       Subject: 'Run test cases',
-      StartTime: new Date(2018, 3, 4, 17, 30),
-      EndTime: new Date(2018, 3, 4, 19, 30),
+      StartTime: new Date(2018, 3, 18, 17, 30),  // April 18th
+      EndTime: new Date(2018, 3, 18, 19, 30),
       IsAllDay: false,
-      ProjectId: 1,
-      TaskId: 2
+      OwnerId: 2,
   }, {
       Id: 70,
       Subject: 'Bug Automation',
-      StartTime: new Date(2018, 3, 4, 18, 30),
-      EndTime: new Date(2018, 3, 4, 20),
+      StartTime: new Date(2018, 3, 20, 18, 30),  // April 20th
+      EndTime: new Date(2018, 3, 20, 20),
       IsAllDay: false,
-      ProjectId: 2,
-      TaskId: 1
+      OwnerId: 3,
   }
 ];
--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,36 @@
 'use client'
 import {
-  Week, Month, Agenda, ScheduleComponent, ViewsDirective, ViewDirective, EventSettingsModel, ResourcesDirective, ResourceDirective, Inject, Resize, DragAndDrop
+  Day, Week, Month, Agenda, ScheduleComponent, ViewsDirective, ViewDirective, EventSettingsModel, ResourcesDirective, ResourceDirective, Inject
 } from '@syncfusion/ej2-react-schedule';
 import { timelineResourceData } from './datasource';
-// import { registerLicense } from '@syncfusion/ej2-base';
+import { registerLicense } from '@syncfusion/ej2-base';
 
 
 export default function Home() {
-  // registerLicense(process.env.SYNCFUSION_LICENSE || '');
+  registerLicense(process.env.SYNCFUSION_LICENSE || '');
   const eventSettings: EventSettingsModel = { dataSource: timelineResourceData }
-  const group = { byGroupID: false, resources: ['Projects', 'Categories'] }
 
-  const projectData: Object[] = [
-    { text: 'PROJECT 1', id: 1, color: '#cb6bb2' },
-    { text: 'PROJECT 2', id: 2, color: '#56ca85' },
-    { text: 'PROJECT 3', id: 3, color: '#df5286' },
+  const ownerData = [
+    { OwnerText: 'Nancy', Id: 1, OwnerColor: '#ffaa00' },
+    { OwnerText: 'Steven', Id: 2, OwnerColor: '#f8a398' },
+    { OwnerText: 'Michael', Id: 3, OwnerColor: '#7499e1' }
   ];
-  const categoryData: Object[] = [
-    { text: 'Development', id: 1, color: '#1aaa55' },
-    { text: 'Testing', id: 2, color: '#7fa900' }
-  ];
+
   return (
     <>
       <h2>Syncfusion React Schedule Component</h2>
-      <ScheduleComponent width='100%' height='550px' currentView='Month' selectedDate={new Date(2018, 3, 4)} eventSettings={eventSettings} group={group} >
+      <ScheduleComponent width='100%' height='550px' selectedDate={new Date(2018, 3, 1)} eventSettings={eventSettings}>
         <ViewsDirective>
+          <ViewDirective option='Day' />
           <ViewDirective option='Week' />
           <ViewDirective option='Month' />
           <ViewDirective option='Agenda' />
         </ViewsDirective>
         <ResourcesDirective>
-          <ResourceDirective field='ProjectId' title='Choose Project' name='Projects' allowMultiple={false}
-            dataSource={projectData} textField='text' idField='id' colorField='color'>
-          </ResourceDirective>
-          <ResourceDirective field='TaskId' title='Category' name='Categories' allowMultiple={true}
-            dataSource={categoryData} textField='text' idField='id' colorField='color'>
+          <ResourceDirective field='OwnerId' title='Owner' name='Owners' allowMultiple={true} dataSource={ownerData} textField='OwnerText' idField='Id' colorField='OwnerColor'>
           </ResourceDirective>
         </ResourcesDirective>
-        <Inject services={[Week, Month, Agenda, Resize, DragAndDrop]} />
+        <Inject services={[Day, Week, Month, Agenda]} />
       </ScheduleComponent>
     </>
   )
