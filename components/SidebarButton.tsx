3676544c5569de6ad2ef6d0536b2ae77fc132934
Jun Ukemori <jukemori@gmail.com>
2024-12-11 17:19:13 +0900

    feat: Apply dropdown menu to sidebar button

--- components/SidebarButton.tsx
+++ components/SidebarButton.tsx
@@ -1,6 +1,12 @@
-import { LucideIcon } from 'lucide-react'
+import { LucideIcon, MoreHorizontal } from 'lucide-react'
 import { Button, ButtonProps } from './ui/button'
 import { cn } from '@/lib/utils'
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from './ui/dropdown-menu'
 interface SidebarButtonProps extends ButtonProps {
   icon?: LucideIcon
 }
@@ -11,13 +17,28 @@ export function SidebarButton({
   ...props
 }: SidebarButtonProps) {
   return (
-    <Button
-      variant="ghost"
-      className={cn('gap-2 justify-start', className)}
-      {...props}
-    >
-      {Icon && <Icon size={20} />}
-      <span>{children}</span>
-    </Button>
+    <DropdownMenu>
+      <Button
+        variant="ghost"
+        className={cn('gap-2 justify-between', className)}
+        {...props}
+      >
+        {Icon && <Icon size={20} />}
+        <span>{children}</span>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-auto p-0 hover:bg-transparent">
+            <MoreHorizontal />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent side="right" align="start" className="rounded-lg">
+          <DropdownMenuItem>
+            <span>Edit Project</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <span>Delete Project</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </Button>
+    </DropdownMenu>
   )
 }
