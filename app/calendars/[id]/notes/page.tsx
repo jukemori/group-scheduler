16b43cfccf9e2fd04b53b4099044b1a62d86a940
Jun Ukemori <jukemori@gmail.com>
2024-12-18 11:41:30 +0900

    feat: Add request to fetch calendar name in notes page

--- app/calendars/[id]/notes/page.tsx
+++ app/calendars/[id]/notes/page.tsx
@@ -1,11 +1,36 @@
 'use client'
 import CalendarNotes from '@/components/CalendarNotes'
 import { useParams, useRouter } from 'next/navigation'
-import { useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export default function NotesPage() {
   const router = useRouter()
   const params = useParams()
+  const [calendarTitle, setCalendarTitle] = useState('')
+  const [calendarId, setCalendarId] = useState('')
+
+  const getAuthHeaders = () => {
+    return {
+      'access-token': localStorage.getItem('access-token') || '',
+      client: localStorage.getItem('client') || '',
+      uid: localStorage.getItem('uid') || '',
+    }
+  }
+
+  const fetchCalendarDetails = useCallback(async () => {
+    try {
+      const response = await fetch(
+        `http://127.0.0.1:3001/api/v1/calendars/${calendarId}`,
+        {
+          headers: getAuthHeaders(),
+        },
+      )
+      const data = await response.json()
+      setCalendarTitle(data.name)
+    } catch (error) {
+      console.error('Error fetching calendar details:', error)
+    }
+  }, [calendarId])
 
   useEffect(() => {
     const accessToken = localStorage.getItem('access-token')
@@ -13,12 +38,22 @@ export default function NotesPage() {
     const uid = localStorage.getItem('uid')
 
     if (!(accessToken && client && uid)) {
-      router.push('/')
+      router.push('/login')
+      return
     }
-  }, [router])
+
+    setCalendarId(params.id as string)
+  }, [router, params.id])
+
+  useEffect(() => {
+    if (calendarId) {
+      fetchCalendarDetails()
+    }
+  }, [calendarId, fetchCalendarDetails])
 
   return (
     <div className="container mx-auto p-4">
+      <h2 className="text-lg font-bold mb-4">{calendarTitle} Notes</h2>
       <CalendarNotes calendarId={params.id as string} />
     </div>
   )
