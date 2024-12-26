d3964d5a412a2ab05251c4ee80468a884a21825f
Jun Ukemori <jukemori@gmail.com>
2024-12-26 17:50:30 +0900

    feat: Add Next-Auth session provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { registerLicense } from '@syncfusion/ej2-base'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import ClientLayout from './client-layout'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ClientLayout>{children}</ClientLayout>
+      </body>
     </html>
   )
 }
